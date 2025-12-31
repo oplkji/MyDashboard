6f1662338fcb0b5d9252f25ed08a2c09ba1272b0
oplkji <105696565+oplkji@users.noreply.github.com>
2025-12-31 21:14:58 +0700

    Change socket server URL in main.js

--- DoAnTotNghiep/main.js
+++ DoAnTotNghiep/main.js
@@ -1,4 +1,4 @@
-const socket = io("https://myserver-xcxt.onrender.com");
+const socket = io("https://myserver-zvlf.onrender.com");
 
 socket.on("init-data", (data) => {
   if (data.node1) updateNode1(data.node1);
